8cba3835a2f11d86690f29ca2df0046f80620b7e
Bianchi <bianchimena1999@gmail.com>
2023-04-02 12:24:03 -0400

    Updated Digital Clock App. Now functional

--- date/digitalClockApp.java
+++ date/digitalClockApp.java
@@ -3,17 +3,13 @@ package date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-//4 Different ways to Get Current Date and Time in java
-//1. SimpleDateFormat (class)
-//2. Date
-//3. LocalDate
-//4. LocalDateTime
 public class digitalClockApp {
 
     public static void main(String[] args) {
 
-        String date = new SimpleDateFormat("yyyy_MM__dd_HH_mm_ss")
+        String date = new SimpleDateFormat("K:mm a, EEE, MMM d, yyyy GGG")
                 .format(Calendar.getInstance().getTime());
         System.out.println(date);
+
     }
 }
